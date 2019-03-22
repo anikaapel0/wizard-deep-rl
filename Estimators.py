bug fixes tf summaries and policy gradient

Estimators.py
@@ -35,7 +35,7 @@ class DQNEstimator(Estimator):
     n_hidden_2 = 512
     n_hidden_3 = 1024
 
-    def __init__(self, session, input_shape, output_shape=Card.DIFFERENT_CARDS, memory=10000, batch_size=1024,
+    def __init__(self, session, input_shape, output_shape=Card.DIFFERENT_CARDS, memory=100000, batch_size=1024,
                  gamma=0.95, target_update=5000, save_update=100000):
         self.input_shape = input_shape
         self.output_shape = output_shape
@@ -60,10 +60,10 @@ class DQNEstimator(Estimator):
         self._init_model()
         self.print_graph()
 
-    def dqn_network(self, input_size, scope_name, act=tf.nn.relu):
+    def dqn_network(self, input, scope_name, act=tf.nn.relu):
 
         with tf.variable_scope(scope_name):
-            hidden1 = tf.layers.dense(input_size, self.n_hidden_1, activation=act, name=scope_name + "_1",
+            hidden1 = tf.layers.dense(input, self.n_hidden_1, activation=act, name=scope_name + "_1",
                                       kernel_initializer=tf.contrib.layers.xavier_initializer())
             hidden2 = tf.layers.dense(hidden1, self.n_hidden_2, activation=act, name=scope_name + "_2",
                                       kernel_initializer=tf.contrib.layers.xavier_initializer())
@@ -79,8 +79,8 @@ class DQNEstimator(Estimator):
             self._x = tf.placeholder("float", [None, self.input_shape], name="state")
             self._y = tf.placeholder("float", [None, self.output_shape], name="output")
 
-        self._prediction = self.dqn_network(input_size=self._x, scope_name="Q_Primary")
-        self._target = self.dqn_network(input_size=self._x, scope_name="Q_Target")
+        self._prediction = self.dqn_network(input=self._x, scope_name="Q_Primary")
+        self._target = self.dqn_network(input=self._x, scope_name="Q_Target")
 
         with tf.variable_scope("Learning_CardPrediction"):
             self._loss = tf.losses.mean_squared_error(self._y, self._prediction)
@@ -88,7 +88,7 @@ class DQNEstimator(Estimator):
 
         tf.summary.scalar('loss_card-prediction', self._loss)
 
-        self._merged = tf.summary.merge_all()
+        self._merged = tf.summary.merge(self._loss)
         self._sum_writer = tf.summary.FileWriter("log/dqn/train-summary", self._session.graph)
         self._var_init = tf.global_variables_initializer()
 

TrickPrediction.py
@@ -40,12 +40,12 @@ class TrickPrediction(object):
             self._trick_prediction = tf.layers.dense(hidden1_trick, self.output_shape, use_bias=False)
 
         with tf.variable_scope("Learning_TrickPrediction"):
-            self._trick_loss = tf.losses.mean_squared_error(self._tricks*10, self._trick_prediction*10)
+            self._trick_loss = tf.losses.mean_squared_error(self._tricks, self._trick_prediction)
             self._trick_optimizer = tf.train.AdamOptimizer(learning_rate=0.005).minimize(self._trick_loss)
 
         tf.summary.scalar('loss_trick-prediction', self._trick_loss)
 
-        self._merged2 = tf.summary.merge_all()
+        self._merged2 = tf.summary.merge(self._trick_loss)
         self._var_init = tf.global_variables_initializer()
 
         self._train_writer = tf.summary.FileWriter("log/trick-prediction/train-summary", self._session.graph)
@@ -97,9 +97,9 @@ class TrickPrediction(object):
             self._tricks: batch_y
         }
         # train network
-        opt, loss = self._session.run([self._trick_optimizer, self._trick_loss], feed_dict)
+        summary, opt, loss = self._session.run([self._merged2, self._trick_optimizer, self._trick_loss], feed_dict)
         print("Epoch {} - Loss: {}".format(self.t_train, loss))
-        # self._train_writer.add_summary(summary, self.t_train)
+        self._train_writer.add_summary(summary, self.t_train)
 
     def collect_training_data(self, players):
         x = None

WizardStatistics.py
@@ -4,11 +4,13 @@ from Wizard import Wizard
 from Featurizers import Featurizer
 from TrickPrediction import TrickPrediction
 from Estimators import DQNEstimator
+from PolicyEstimator import PolicyGradient
 
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+import time
 import gc
 import psutil
 
@@ -75,7 +77,8 @@ class WizardStatistic(object):
 
         ax.set(xlabel='Number of rounds played', ylabel='Percentage of won games')
         ax.legend()
-        plt.show()
+        # plt.show()
+        plt.savefig('log/statistics/' + time.strftime("%Y-%m-%d_%H-%M-%S") + ".png")
 
     def get_playertype(self, player):
         if isinstance(player, RLAgent):
@@ -98,29 +101,33 @@ if __name__ == "__main__":
         # tf.reset_default_graph()
         featurizer = Featurizer()
         estimator = DQNEstimator(session=sess, input_shape=featurizer.get_state_size())
+        pg_estimator = PolicyGradient(session=sess, input_shape=featurizer.get_state_size())
+
         trick_predictor = TrickPrediction(session=sess)
         init = tf.global_variables_initializer()
-        print(init)
         sess.run(tf.global_variables_initializer())
 
         writer = tf.summary.FileWriter("log/graph", sess.graph)
         writer.close()
 
-        players = [AverageRandomPlayer(), AverageRandomPlayer(),
-                   RLAgent(estimator=estimator, featurizer=featurizer, trick_prediction=trick_predictor),
-                   RLAgent(estimator=estimator, featurizer=featurizer)]
+        players2 = [AverageRandomPlayer(), AverageRandomPlayer(), AverageRandomPlayer(),
+                    RLAgent(estimator=pg_estimator, featurizer=featurizer)]
 
-        stat = WizardStatistic(num_games=5000, num_agents=2, players=players)
+        # players = [RLAgent(estimator=estimator, featurizer=featurizer) for _ in range(4)]
+
+        stat = WizardStatistic(num_games=50000, num_agents=1, players=players2)
         stat.play_games()
         stat.plot_game_statistics()
+
+
 #        finally:
 #            if stat is not None:
 #                stat.close()
 
-    print(psutil.cpu_percent())
-    print(psutil.virtual_memory())  # physical memory usage
-    gc.collect()
-    print("After collecting:")
-    print(psutil.virtual_memory())  # physical memory usage
+    # print(psutil.cpu_percent())
+    # print(psutil.virtual_memory())  # physical memory usage
+    # gc.collect()
+    # print("After collecting:")
+    # print(psutil.virtual_memory())  # physical memory usage
 
 